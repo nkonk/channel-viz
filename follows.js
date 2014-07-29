2f0fedfc26ec2b2d24a003aaeda6b03f43d07be5
nkonk <nkonk@users.noreply.github.com>
2014-07-29 03:02:55 -0400

    Update follows.js
    added apikey

--- follows.js
+++ follows.js
@@ -12,11 +12,11 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'SYanm3TK3sKANXcrTdQ58kblmTxWcTz39FoVnTlKVsZvIvNM', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [261563333],// Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataInterval	= 1, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
